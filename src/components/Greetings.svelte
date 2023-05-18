e53fece61499d99fa6e82208f3c2bbb747b7a8df
prosenjitjoy <prosen.joy@gmail.com>
2023-05-18 17:30:21 +0600

    complete

---
+++ src/components/Greetings.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let messages: string[];
+    const randomMessage = () => {
+        const randomIndex = Math.floor(Math.random() * messages.length);
+        greeting = messages[randomIndex];
+    };
+    $: greeting = messages[0];
+</script>
+
+<div>
+    <h3>{greeting}! Thank you for visiting</h3>
+    <button on:click={randomMessage}>New Greeting</button>
+</div>
+
+<style>
+    button {
+        padding: 0.5rem 1rem;
+        color: white;
+        background-color: #4c1d95;
+        text-decoration: none;
+    }
+</style>
